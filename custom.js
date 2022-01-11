a7e36b442bb03ab97394bf67f696f2e5b52688c9
media-sign <97297045+media-sign@users.noreply.github.com>
2022-01-11 16:38:56 +0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -2,5 +2,11 @@ Ecwid.OnPageLoaded.add(function(page){
   console.log("Current page is of type: " + page.type);
   if(page.type == 'CHECKOUT_ADDRESS'){
     console.log('y')
+    adress1 = document.querySelector('#ec-address-line1');
+    adress1.value = 'Москва';
+    adress1.dispatchEvent(new Event('input', { bubbles: true }));
+    code = document.querySelector('#ec-postal-code');
+    code.value = '101000';
+    code.dispatchEvent(new Event('input', { bubbles: true }));
   }
 });
